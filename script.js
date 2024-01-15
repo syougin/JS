120c3f5533b471353d8944d1e9a6efb7d52c7a29
shu yin <shuyin1994916@outlook.com>
2024-01-15 13:10:46 +0800

    Splice(start poistion,length,added item)

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const numbers = [1,2,3,4,5]
-console.log(numbers.reverse());
-console.log(numbers.slice()) // -> it copies all  item
-console.log(numbers.slice(0)) // -> it copies all  item
-console.log(numbers.slice(0, numbers.length)) // it copies all  item
-console.log(numbers.slice(1,4)) // -> [2,3,4] // it doesn't include the ending position
+const numbers = [1, 2, 3, 4, 5, 6]
+numbers.splice(3, 3, 7, 8, 9)
+console.log(numbers.splice(3, 3, 7, 8, 9))  // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
