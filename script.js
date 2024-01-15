16f5fe2c38ab4cdc4a1ad1b387fd5a96c0924709
shu yin <shuyin1994916@outlook.com>
2024-01-15 13:04:58 +0800

    Descending

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const numbers = [1,2,3,4,5]
-
+console.log(numbers.reverse());
 console.log(numbers.slice()) // -> it copies all  item
 console.log(numbers.slice(0)) // -> it copies all  item
 console.log(numbers.slice(0, numbers.length)) // it copies all  item
